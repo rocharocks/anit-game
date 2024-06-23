2bf2b82ec73610597bb4b9cde54a48268ceae4ea
rocharocks <162216750+rocharocks@users.noreply.github.com>
2024-06-23 18:37:18 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('gameCanvas');
+const canvas = document.getElementById('gameCanvas'); 
 const ctx = canvas.getContext('2d');
 
 const playerImage = new Image();
