c45132e7a18111459a74b63b7e6bb7765e885977
rocharocks <162216750+rocharocks@users.noreply.github.com>
2024-06-23 19:24:41 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-function resizeCanvas() {
+function resizeCanvas() { 
     const aspectRatio = 2 / 1;
     const width = window.innerWidth;
     const height = window.innerHeight;
